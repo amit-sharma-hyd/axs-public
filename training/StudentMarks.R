731c23a0d29b9b8ce7caba954c0768597d4ae706
Amit Sharma <Amit.Sharma@adp.com>
2016-08-06 00:55:01 +0530

    Modified the data file

--- training/StudentMarks.R
+++ training/StudentMarks.R
@@ -17,27 +17,26 @@ str(stMarksDF)
 stMarksDF = stMarksDF[complete.cases(stMarksDF),]
 
 
-# Find total Marks for each student
-colfunc <- colorRampPalette(c("green", "orange"))
-par(las=2) 
-stTotalMarks = apply(marksDF,1,sum)
-stRank = stTotalMarks[order(stTotalMarks, decreasing = T)]
-barplot(stRank[1:10], names=stMarksDF[names(stRank[1:10]),]$Roll.No., col=colfunc(10))
-
-# Find the topper
-
 # Take out Roll Nos for the time being
 marksDF = subset(stMarksDF, select=-c(Roll.No.))
+# Summarize the data in the marks DF
+summary(marksDF)
 
 # Tabulate pass vs fail
 passFail = table(marksDF>=40)
 pie(passFail, col = c('red', 'blue'), main="Passed Vs Failed", cex=0.8,
     labels=paste0(names(passFail), " - ", passFail))
 
-table(any(marksDF))
+# Find total Marks for each student
+stTotalMarks = apply(marksDF,1,sum)
 
-# Summarize the data in the marks DF
-summary(marksDF)
+# Find the topper
+colfunc <- colorRampPalette(c("green", "yellow", "red"))
+par(las=2) 
+N = 20
+stRank = stTotalMarks[order(stTotalMarks, decreasing = T)]
+barplot(stRank[1:N], names=stMarksDF[names(stRank[1:N]),]$Roll.No., col=colfunc(N))
+
 
 # Set plot window to single chart
 par(mfrow=c(1,1))
@@ -45,7 +44,7 @@ par(mfrow=c(1,1))
 # Checking range and variance of marks across subjects
 boxplot(marksDF, outline=F, col=rainbow(12:14))
 
-# Check the mean of each student
+# Check the mean of each subject
 barplot(sapply(marksDF, mean), col=rainbow(11:14))
 
 # Check correlation of marks
@@ -55,7 +54,7 @@ cr = round(cor(marksDF), 1)
 library(corrplot)
 corrplot(cr)
 # Check correlation plot for mtcars
-corrplot(cor(mtcars), type="lower", method="circle")
+# corrplot(cor(mtcars), type="lower", method="circle")
 
 # Histogram plots
 hist(stMarksDF$MEFA, col=rainbow(12:14))
@@ -69,3 +68,55 @@ for (i in seq(1,8)) {
 par(mfrow=c(2,1))
 boxplot(marksDF, outline=F, col=rainbow(12:14))
 barplot(sapply(marksDF, mean), col=rainbow(11:14))
+
+#########################
+# REGRESSSION ANALYSIS  #
+#########################
+
+# Lets take some students as test set and remaining as test
+N = nrow(marksDF)
+trainRows = sample(1:N, N*0.9, replace=F)
+train = marksDF[trainRows,]
+test = marksDF[-trainRows,]
+
+# Now lets fit the model
+head(train)
+# Let's make PS score as output variable and all others input
+model = lm(MFCS~.,train)
+summary(model)
+preds = predict(model, newdata = test)
+res = data.frame(test$PS, round(preds,0))
+res
+names(res) = c("Actual", "Pred")
+mse = sum((res$Actual-res$Pred)^2)/nrow(res)
+mse
+# Is the MSE any better than mean of train output
+sum((res$Actual-mean(train$MFCS))^2)/nrow(res)
+
+
+#########################
+# LOGISTIC REGRESSSION  #
+#########################
+
+# Lets find a good cutoff for marks to be classified as Great Score (1) or not (0)
+summary(marksDF$MFCS)
+table(marksDF$MFCS>70)
+marksDF$MFCS_Great_Score = ifelse(marksDF$MFCS>70, 1, 0)
+
+# Now we shall remove the MFCS col to remove direct calculation
+df = marksDF
+df$MFCS = NULL
+
+# Lets take some students as test set and remaining as test
+N = nrow(df)
+trainRows = sample(1:N, N*0.9, replace=F)
+train = df[trainRows,]
+test = df[-trainRows,]
+
+model <- glm(MFCS_Great_Score ~.,family=binomial(link='logit'),data=train)
+
+summary(model)
+preds = predict(model, newdata = test, type = "response")
+res = data.frame(test$MFCS_Great_Score, round(preds,0))
+names(res) = c("Actual", "Pred")
+res
